7974ee0a227a2ed5176ab7da36205336037d640c
Yvan Gauthier <y.gauthier@keemail.me>
2024-09-05 09:57:31 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,45 @@
+// Gessing game 
+// date : 04/08/2024 
+// version : 0.0.1 
+
+#include<iostream> 
+using namespace std; 
+
+// Function declaration 
+	void user_input() {
+		// Code 
+		// Ask is the user want to play or no. 
+		cout<<"Hello, would you like to play a game y or n : "; 
+		char i; 
+		cout<<"
+
+		cin >> i;
+
+	}
+// Function Hard level
+// The hard level have 5 try 
+	void hard_level() {
+		// Code 
+		cout<<"Function 2\n"; 
+	}
+// Function Medium level 
+// The medium level have 10 try 
+	void medium_level(){
+		// Code 
+		cout<<"Function 3\n"; 
+	}
+
+// Funtion easy level 
+// The easy level have 15 try 
+	void easy_level(){
+		// Code 
+		cout<<"Function 4\n"; 
+	}
+
+// This is simple game written in C++
+// The main function 
+int main() {
+	// Test 
+	user_input();  
+return 0; 
+}
